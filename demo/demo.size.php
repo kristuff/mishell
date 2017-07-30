cf1891e34313c7d3c06db85bf6be0b6c989467e2
kristuff <christophe.buliard@outlook.com>
2017-07-30 21:45:46 +0200

    dev

--- demo/demo.size.php
+++ demo/demo.size.php
@@ -10,8 +10,14 @@ Console::log('');
 Console::log('Sample:', 'underline', 'bold');
 Console::log('');
 
+// get columns and lines 
 $lines = Console::getLines();
 $cols = Console::getColumns();
 
+// print value
 Console::log('The number of lines is currently: ' .Console::text($lines, 'green'));
 Console::log('The number of columns is currently: ' .Console::text($cols, 'green'));
+Console::log('');
+
+// build a 'full' row
+Console::log(Console::pad('I have exactly the length of your terminal.', $cols), 'white', 'yellow');
